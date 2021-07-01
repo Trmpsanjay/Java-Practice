9fd94906d1cda0392c4c35cbe3d77434e38fa53b
trmpsanjay <trmpsanjay@gmail.com>
2021-07-01 10:40:20 +0530

    reverse LL recursive added

--- LinkedList/reverseLinkedKistRecursive.java
+++ LinkedList/reverseLinkedKistRecursive.java
@@ -20,12 +20,39 @@ public class Solution
 {
 	public static Node reverseLinkedList(Node head) 
 	{
-        if(head==null || head.next == null)
+//         // Recursive solutuin
+//         if(head==null || head.next == null)
+//             return head;
+       
+//         Node reversed = reverseLinkedList(head.next);
+//          Node temp = head.next;;
+//         temp.next = head;
+//         head.next = null;
+       
+//         return reversed;
+        
+
+
+//         // Fix the head pointer
+//         return rest;
+        
+        
+        // non recursive solution
+        if(head == null || head.next == null)
             return head;
-        Node temp = head.next;
-        Node reversed = reverseLinkedList(head.next);
-        head.next = null;
-        temp.next = head;
-        return reversed;
+        Node curr = head;
+        Node prev = null;
+        Node temp ;
+        while(curr!=null){
+            temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+            
+        }
+        return prev;
+        
+        
+        
 	}
 }
